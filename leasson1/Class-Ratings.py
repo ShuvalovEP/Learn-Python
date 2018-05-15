75694c89a4d842731059136ef140fe53c0626a77
Shuvalov Evgeny <ShuvalovEP@gmail.com>
2018-05-15 10:24:56 +0300

    Почти закончил задание Оценки

--- leasson1/Class-Ratings.py
+++ leasson1/Class-Ratings.py
@@ -1,8 +1,8 @@
-ratings = [{'school_class': '5a', 'scores': [2,3,5,4,5]},
+ratings = [{'school_class': '5a', 'scores': [2,3,5,2,5]},
            {'school_class': '6a', 'scores': [3,4,4,3,4]},
            {'school_class': '7a', 'scores': [4,5,3,2,3]},
-           {'school_class': '8a', 'scores': [5,2,2,5,2]},
-           {'school_class': '5b', 'scores': [2,3,5,4,5]},
+           {'school_class': '8a', 'scores': [5,2,2,3,2]},
+           {'school_class': '5b', 'scores': [2,3,5,2,5]},
            {'school_class': '6b', 'scores': [3,4,4,4,4]},
            {'school_class': '7b', 'scores': [4,5,3,3,3]},
            {'school_class': '8b', 'scores': [5,2,2,2,2]},
@@ -18,10 +18,13 @@ def average_score_class():
     id = 0 
     for school_class in ratings: 
         count_score = 0
+        len_scores = len(school_class['scores'])
         for scores in school_class['scores']:
             count_score += scores
-            ratings[id]['count_score'] = count_score
+            ratings[id]['count_score'] = count_score / len_scores
         id += 1
+        print('Класс: ', school_class['school_class'], 'Средний бал: ', school_class['count_score'])
+
 
 average_score_class()
 
