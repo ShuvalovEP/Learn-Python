c6e1e9aa8f66f97b2b97bc56db977e3bf881a6f3
Shuvalov Evgeny <ShuvalovEP@gmail.com>
2018-05-15 14:36:54 +0300

    Задача Где Валера

--- leasson1/Dora_the_Explorer.py
+++ leasson1/Dora_the_Explorer.py
@@ -11,17 +11,3 @@ while True:
     else:
         print('Ищу Валеру')
 
-# Перепишите предыдущий пример в виде функции find_person(name), которая ищет имя в списке.
-people_list = ["Ваня", "Маша", "Петя", "Валера", "Вася", "Саша", "Даша"]
-def find_person(name):
-    for person in people_list:
-        if person == name:
-            print(person, 'под номером: ', people_list.index(person))
-
-
-find_person("Вася")
-
-
-
-# Написать функцию ask_user() чтобы с помощью input() спрашивать пользователя “Как дела?”, пока он не ответит “Хорошо”
-# При помощи функции get_answer() отвечать на вопросы пользователя в ask_user(), пока он не скажет “Пока!”
