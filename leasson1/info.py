ea1ba245e6f8ca4f4f3b65a52fd6f76626e7ee61
Shuvalov Evgeny <ShuvalovEP@gmail.com>
2018-05-15 11:57:25 +0300

    Изучаем input()

--- leasson1/info.py
+++ leasson1/info.py
@@ -1,5 +1,5 @@
 user_info  = {}
-user_info['first_name'] = input('Введите фамилию: ')
-user_info['last_name'] = input('Введите имя: ')
+user_info['first_name'] = input('Введите Вашу фамилию: ')
+user_info['last_name'] = input('Введите Ваше имя: ')
 
 print('-' * 30, '\nФамилия: ', user_info['first_name'], '\nИмя: ', user_info['last_name'])
