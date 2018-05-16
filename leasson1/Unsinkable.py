54d1ce5d0948f6a9b345c2797e7880e3f6d6442e
Shuvalov Evgeny <ShuvalovEP@gmail.com>
2018-05-16 14:10:24 +0300

    Задание обработка exception

--- leasson1/Unsinkable.py
+++ leasson1/Unsinkable.py
@@ -1,2 +1,26 @@
-#Переписать функцию ask_user(), добавив обработку exception-ов
-#Добавить перехват ctrl+C и прощание
+# Переписать функцию ask_user(), добавив обработку exception-ов
+def get_answer(question):
+    try:
+        answers = {"Как дела": "Лучше всех", 
+                   "Сколько тебе лет": "31", 
+                   "Чем занимаешся": "Да ничем"}
+        if question in answers:
+            return answers.get(question)
+        else:
+            return 'Ты о чем?'
+    except:
+        return('Что то пошло не так')
+
+def ask_user():
+    try:
+        answer = 0
+        while True: 
+            answer = input()
+            if answer == 'Пока':
+                print('Приятно было поболтать.')
+                break
+            else:
+                print(get_answer(answer))
+    except KeyboardInterrupt: # Добавить перехват ctrl+C и прощание
+        print('Вы нажали сочетание клавиш: ctrl+c')
+        exit()
