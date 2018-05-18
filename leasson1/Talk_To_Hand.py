5a76f6f7ced63508425967237fa167c371635a0d
Shuvalov Evgeny <ShuvalovEP@gmail.com>
2018-05-18 09:59:39 +0300

    Работа над ошибками - Задача Поговори с ботом

--- leasson1/Talk_To_Hand.py
+++ leasson1/Talk_To_Hand.py
@@ -1,6 +1,5 @@
 # Написать функцию ask_user() чтобы с помощью input() спрашивать пользователя “Как дела?”, пока он не ответит “Хорошо”
 def ask_user():
-    answer = 0
     while True: 
         answer = input('Как дела?')
         if answer == 'Хорошо':
@@ -11,7 +10,6 @@ ask_user()
 
 
 # При помощи функции get_answer() отвечать на вопросы пользователя в ask_user(),пока он не скажет “Пока!”
-
 def get_answer(question):
     answers = {"Как дела": "Лучше всех", 
                "Сколько тебе лет": "31", 
@@ -21,8 +19,8 @@ def get_answer(question):
     else:
         return 'Ты о чем?'
 
+
 def ask_user():
-    answer = 0
     while True: 
         answer = input()
         if answer == 'Пока':
@@ -30,9 +28,3 @@ def ask_user():
             break
         else:
             print(get_answer(answer))
-
-
-
-
-
-
