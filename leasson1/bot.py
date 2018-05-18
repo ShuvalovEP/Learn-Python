07a959639a6fcad697dea95d6fee89da95bc4435
Shuvalov Evgeny <ShuvalovEP@gmail.com>
2018-05-18 11:57:31 +0300

    Работа над ошибками - Задача В каком созвездии палнета

--- leasson1/bot.py
+++ leasson1/bot.py
@@ -4,7 +4,9 @@ import logging
 import datetime
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080','urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
+PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
+         'urllib3_proxy_kwargs': {'username': 'learn', 
+                                  'password': 'python'}}
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
@@ -12,48 +14,14 @@ logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
 
 what_day = datetime.datetime.now()
 
-def talk_to_me(bot, update):
-    planet = update.message.text 
-    if planet == 'merkury':
-        planet = ephem.Mercury(what_day)
-        planet = ephem.constellation(planet)
-        update.message.reply_text(planet[1])
-    elif planet == 'wenus':
-        planet = ephem.Venus(what_day)
-        planet = ephem.constellation(planet)
-        update.message.reply_text(planet[1])
-    elif update == 'mars':
-        planet = ephem.Mars(what_day)
-        planet = ephem.constellation(planet)
-        update.message.reply_text(planet[1])
-    elif update == 'jupiter':
-        planet = ephem.Jupiter(what_day)
-        planet = ephem.constellation(planet)
-        update.message.reply_text(planet[1])
-    elif update == 'saturn':
-        planet = ephem.Saturn(what_day)
-        planet = ephem.constellation(planet)
-        update.message.reply_text(planet[1])
-    elif planet == 'uran':
-        planet = ephem.Uranus(what_day)
-        planet = ephem.constellation(planet)
-        update.message.reply_text(planet[1])
-    elif planet == 'neptun':
-        planet = ephem.Neptune(what_day)
-        planet = ephem.constellation(planet)
-        update.message.reply_text(planet[1])
-    elif planet == 'pluton':
-        planet = ephem.Pluto(what_day)
-        planet = ephem.constellation(planet)
-        update.message.reply_text(planet[1])
-    elif planet == 'moon':
-        planet = ephem.Moon(what_day)
-        planet = ephem.constellation(planet)
-        update.message.reply_text(planet[1])
+def get_planet(bot, update):
+    planet = getattr(ephem, update)
+    planet = ephem.constellation(planet(what_day))
+    update.message.reply_text(planet[1])
 
 
 def main():
-    key = os.getenv('telegram_key', '')
+    key = os.getenv('telegram_key', ' ')
     mybot = Updater(key, request_kwargs=PROXY)
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('planet', talk_to_me))
