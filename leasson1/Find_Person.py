67f856e96e34deb694b9a84d56e7f75e025d9610
Shuvalov Evgeny <ShuvalovEP@gmail.com>
2018-05-18 10:51:46 +0300

    Работа над ошибками - Задача Найти человека

--- leasson1/Find_Person.py
+++ leasson1/Find_Person.py
@@ -1,11 +1,11 @@
-# Перепишите предыдущий пример в виде функции find_person(name), которая ищет имя в списке.
+
 people_list = ["Ваня", "Маша", "Петя", "Валера", "Вася", "Саша", "Даша"]
 
 
 def find_person(name):
     for person in people_list:
         if person == name:
-            print(person, 'под номером: ', people_list.index(person))
+            print('{find_name} под номером: '.format(find_name = name), people_list.index(person))
 
 
 find_person("Вася")
