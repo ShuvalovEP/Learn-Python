330b0f5f1b819acceba61fa219043b0201d3b553
Shuvalov Evgeny <ShuvalovEP@gmail.com>
2018-05-21 17:21:35 +0300

    Задача - Научиться работать с датой и временем

---
+++ leasson2/Back-to-the-Future.py
@@ -0,0 +1,50 @@
+from datetime import timedelta, datetime
+
+delta_list = {'yesterday':timedelta(days=1), 'month_ago':timedelta(weeks=4), 'now_day':datetime.now()}
+date_string = '01/01/17 12:10:03.234567'
+
+
+def converting_date_display(args):
+    args = args.strftime('%d.%m.%y')
+    return args
+
+
+def delta_date(args):
+    get_delta = delta_list.get(args)
+    get_now_day = delta_list.get('now_day')
+    calculation_time = get_now_day - get_delta
+    convert = converting_date_display(calculation_time)
+    return convert 
+
+
+def transformation_date(args):
+    transformation = datetime.strptime(args, '%d/%m/%y %H:%M:%S.%f')
+    return transformation 
+
+
+def now_date():
+    date = delta_list.get('now_day')
+    convert = converting_date_display(date)
+    return convert
+
+def main():
+    try:
+        while True:
+            print(' \n Нажмите 1 - Вчера\n Нажмите 2 - Сегодня\n Нажмите 3 - Месяц назад\n Нажмите 4 - Преобразовать: 01/01/17 12:10:03.234567')
+            print(' \n Для выхода нажмите ctrl+c\n')
+            command = input(' Введите номер команды: ')
+            if command == '1':
+                print('\n* - Вчера было:', delta_date('yesterday'))
+            elif command == '2':
+                print('\n* - Сегодня:', now_date())
+            elif command == '3':
+                print('\n* - Месяц назад:', delta_date('month_ago'))
+            elif command == '4':
+                print('\n* - Преобразовано в datetime:', transformation_date(date_string))
+            else:
+                print('{} - неверная команда, команда обязательно должна быть числом'.format(command))
+    except KeyboardInterrupt:
+        print('\n   Пока, пока!')
+        exit()
+
+main()
