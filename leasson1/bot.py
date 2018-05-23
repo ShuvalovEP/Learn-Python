63ce46acdf0cd48bf506296e9f63238a7a358adb
Shuvalov Evgeny <ShuvalovEP@gmail.com>
2018-05-23 14:42:25 +0300

    Работа над ошибками - Задача В каком созвездии палнета

--- leasson1/bot.py
+++ leasson1/bot.py
@@ -4,7 +4,10 @@ import logging
 import datetime
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080', 'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
+#PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080', 'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
+
+PROXY = {'proxy_url': 'socks5://52.15.41.251:80', 'urllib3_proxy_kwargs': {'username': 'junked', 'password': '$huv@lovZ8'}}
+
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
@@ -13,20 +16,45 @@ logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
 what_day = datetime.datetime.now()
 
 
-def get_planet(bot, update):
-    text = update.message.text
+def  get_planet(bot, update, args):
+    text = args[0]
     planet = getattr(ephem, text)
     planet = planet(what_day)
     planet = ephem.constellation(planet)
     update.message.reply_text(planet[1])
- 
+
+
+def word_count(bot, update, args):
+    if args[0].startswith('"') and args[-1].endswith('"'):
+        len_word = len(args)
+        update.message.reply_text(len_word)
+
+
+def calc(bot, update, args):
+    print(args)
+    element_a = int(args[0].strip())
+    usr_operation = args[1]
+    element_b = int(args[-1].strip())
+    lambda_operation = {
+        '-': lambda a,b: a - b,
+        '+': lambda a,b: a + b,
+        '*': lambda a,b: a * b, 
+        '/': lambda a,b: a / b}
+    operation = lambda_operation.get(usr_operation)
+    if element_a == 0 or element_b == 0:
+        update.message.reply_text('division by zero')
+    else:
+        calcula = operation(element_a, element_b)
+        update.message.reply_text(calcula)
+
 
 def main():
     key = os.getenv('telegram_key', ' ')
     mybot = Updater(key, request_kwargs=PROXY)
     dp = mybot.dispatcher
-    dp.add_handler(CommandHandler('planet', get_planet))
-    dp.add_handler(MessageHandler(Filters.text, get_planet))
+    dp.add_handler(CommandHandler('calc', calc, pass_args=True))
+    dp.add_handler(CommandHandler('planet', get_planet, pass_args=True))
+    dp.add_handler(CommandHandler('wordcount', word_count, pass_args=True))
     mybot.start_polling()
     mybot.idle()
 
