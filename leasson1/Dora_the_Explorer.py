ba9a9299806bebb733c594ca3cdfe6b98bf6f138
Shuvalov Evgeny <ShuvalovEP@gmail.com>
2018-06-20 11:10:09 +0300

    Задание - цикл while

--- leasson1/Dora_the_Explorer.py
+++ leasson1/Dora_the_Explorer.py
@@ -1,13 +1,15 @@
-# Пройдите в цикле по списку ["Вася", "Маша", "Петя", "Валера", "Саша", "Даша"] пока не встретите имя "Валера". 
-# Когда найдете напишите "Валера нашелся". Подсказка: используйте метод list.pop()
+# Пройдите в цикле по списку ['Вася', 'Маша', 'Петя', 'Валера', 'Саша', 'Даша'] пока не встретите имя 'Валера'. 
+# Когда найдете напишите 'Валера нашелся'. Подсказка: используйте метод list.pop()
 
-people_list = ["Ваня", "Маша", "Петя", "Валера", "Вася", "Саша", "Даша"]
-sought_man = "Валера"
+people_list = ['Ваня', 'Маша', 'Петя', 'Валера', 'Вася', 'Саша', 'Даша']
 
-while people_list:
-    if people_list.pop() == sought_man:
-        print('{name} нашелся!'.format(name=sought_man))
-        break
-    else:
-        print('{name}, ты где?'.format(name=sought_man))
+def dora(sought_man):
+    while people_list:
+        if people_list.pop() == sought_man:
+            print('{name} нашелся!'.format(name=sought_man))
+            break
+        else:
+            print('{name}, ты где?'.format(name=sought_man))
 
+if __name__ == '__main__':
+    dora('Валера')
