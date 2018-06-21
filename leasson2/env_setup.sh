204569f907b8662a4d00b1f8cf66221341343cd2
Shuvalov Evgeny <ShuvalovEP@gmail.com>
2018-06-21 09:19:38 +0300

    Задача - Научиться использовать virtualenv

--- leasson2/env_setup.sh
+++ leasson2/env_setup.sh
@@ -1,11 +1,11 @@
 #! /usr/bin/env bash
 
 if [ -e ./env ]; then
-	echo "Удаление старого env"
+	echo "Удаление старого env окружения"
 	rm -rf env >> log.txt 2>&1
-	echo "Создание нового env"
+	echo "Создание нового env окружения"
 	python3 -m venv env >> log.txt 2>&1
 else
-	echo "Создание env"
+	echo "Создание нового env окружения"
 	python3 -m venv env >> log.txt 2>&1
 fi
