87aa515831ede1d443c86eced3e4ea8895399509
Shuvalov Evgeny <ShuvalovEP@gmail.com>
2018-06-21 09:18:13 +0300

    Задача - Записать словарь в .csv файл

--- leasson2/Pulp_Fiction.py
+++ leasson2/Pulp_Fiction.py
@@ -1,8 +1,14 @@
+#Pulp_Fiction.py
 import csv
 
 answers_dict = {'Привет': 'И тебе привет!', 'Как дела': 'Лучше всех',
                 'Пока': 'Увидимся', 'Как тебя зовут': 'Mr. Junked'}
 
-with open('answer.csv', 'w',  newline='') as file:
-    write = csv.writer(file, delimiter=';')
-    write.writerows(answers_dict.items())
+def csv_writer():
+    with open('answer.csv', 'w',  newline='') as file:
+        write = csv.writer(file, delimiter=';')
+        write.writerows(answers_dict.items())
+
+
+if __name__ == '__main__':
+    csv_writer()
